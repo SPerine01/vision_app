08173ede83075b5e069874ba5f749d9a1705668b
Sumayyah Perine <sperine01@gmail.com>
2016-01-11 10:42:49 -0500

    more front end styling

--- app/models/vision.rb
+++ app/models/vision.rb
@@ -1,3 +1,6 @@
 class Vision < ActiveRecord::Base
 	belongs_to :user
+	
+	has_attached_file :photo, styles: { medium: "300x300>" }
+  	validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
