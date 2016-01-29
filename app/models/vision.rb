59becd71cb2d75a06b70c56bfb44f5426a5403fd
Sumayyah Perine <sperine01@gmail.com>
2016-01-29 14:49:30 -0500

    styling registrations form

--- app/models/vision.rb
+++ app/models/vision.rb
@@ -3,4 +3,5 @@ class Vision < ActiveRecord::Base
 	
 	has_attached_file :photo, styles: { medium: "300x300>" }
   	validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+  	validates :photo, attachment_presence: true
 end
